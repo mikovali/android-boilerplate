c53444b8b7ace6cd36c3a41611727a2eb50af7e5
Miko Väli <mikovali@gmail.com>
2017-10-27 15:11:02 +0300

    Test fails

---
+++ src/main/java/io/github/mikovali/boilerplate/android/User.java
@@ -0,0 +1,14 @@
+package io.github.mikovali.boilerplate.android;
+
+public final class User {
+
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return null;
+    }
+}
