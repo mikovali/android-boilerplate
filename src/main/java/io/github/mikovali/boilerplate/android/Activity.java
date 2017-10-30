4f1a52e2d4bc34329e622e3806e4519d3b5dfab2
Miko Väli <mikovali@gmail.com>
2017-10-30 11:11:00 +0200

    NDK

--- src/main/java/io/github/mikovali/boilerplate/android/Activity.java
+++ src/main/java/io/github/mikovali/boilerplate/android/Activity.java
@@ -1,4 +1,13 @@
 package io.github.mikovali.boilerplate.android;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 public class Activity extends android.app.Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TestingLibBridge.testing();
+    }
 }
